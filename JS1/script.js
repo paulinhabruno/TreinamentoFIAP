598621b8a7c60d36dd9cdec4e50227ffe350c17a
paulinhabruno <mapalurj@gmail.com>
2022-01-03 16:15:04 -0300

    inicio projeto cpf

---
+++ JS1/script.js
@@ -0,0 +1,44 @@
+const inputCpf = document.querySelector("#inputCpf");
+
+const handeCpfInput = (event) => {
+
+    alert(validaCpf(event.target.value));
+
+}
+
+inputCpf.addEventListener("input", handeCpfInput);
+
+const mascaraCpf = (cpf) => {
+    return cpf
+      ? cpf
+          .replace(/\D/g, "")
+          .replace(/(\d{3})(\d)/, "$1.$2")
+          .replace(/(\d{3})(\d)/, "$1.$2")
+          .replace(/(\d{3})(\d{1,2})/, "$1-$2")
+          .replace(/(-\d{2})\d+?$/, "$1")
+      : "";
+  };
+
+const validaCpf = (cpf) => {
+    var soma;
+    var resto;
+    soma = 0;
+    if (cpf == "00000000000") return false;
+  
+    for (i = 1; i <= 9; i++) 
+          soma = soma + parseInt(cpf.substring(i - 1, i)) * (11 - i);
+  
+    resto = (soma * 10) % 11;
+  
+    if ((resto == 10) || (resto == 11)) resto = 0;
+    if (resto != parseInt(cpf.substring(9, 10))) return false;
+  
+    soma = 0;
+    for (i = 1; i <= 10; i++) soma = soma + parseInt(cpf.substring(i - 1, i)) * (12 - i);
+    resto = (soma * 10) % 11;
+  
+    if ((resto == 10) || (resto == 11)) resto = 0;
+    if (resto != parseInt(cpf.substring(10, 11))) return false;
+    return true;
+  }
+  
