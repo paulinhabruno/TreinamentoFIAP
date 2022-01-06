807f493644006a4a564dcef4194247ff2aec3789
paulinhabruno <mapalurj@gmail.com>
2022-01-06 12:00:11 -0300

    finalização do projeto de listas

--- JS2/script.js
+++ JS2/script.js
@@ -1,3 +1,8 @@
+/**
+ * Vamos estruturar as nossas notas
+ * Daqui pra frente terão id e a data de inclusão
+ */
+
 const form = document.querySelector("#form-notes");
 const inputCpf = document.querySelector("#input-cpf");
 const sectionListNotes = document.querySelector("#list-notes");
@@ -6,66 +11,86 @@ const KEY_STORAGE = "@NotesAvanade";
 
 let listNotes = [];
 
+
 const saveNotesToStorage = () =>
- localStorage.setItem(KEY_STORAGE, JSON.stringify(listNotes));
+  localStorage.setItem(KEY_STORAGE, JSON.stringify(listNotes));
+
+const loadNotesFromStorage = () => {
+  const listStorage = JSON.parse(localStorage.getItem(KEY_STORAGE));
+
+  listNotes = listStorage || [];
+
+  //convertendo a data string para o formato Date()
+  listNotes.forEach(note => {
+    note.date = new Date (note.date);
+    addNoteToList(note);
+  });
+}
 
 window.addEventListener("unload", saveNotesToStorage);
 
-const loadNotesFromStorage = () =>{
-    
-    const listStorage = JSON.parse(localStorage.getItem(KEY_STORAGE));
-    
-    listNotes = listStorage;
+window.addEventListener("load", loadNotesFromStorage);
 
-    listNotes.forEach(note => addNoteToList(note));
+const removeNote = (event, idnoteToRemove) => {
+  const noteToRemove = event.target.parentNode;
+  sectionListNotes.removeChild(noteToRemove);
+
+  listNotes = listNotes.filter(note => note.id !== idnoteToRemove)
 
 }
 
-window.addEventListener("load", loadNotesFromStorage);
-
-const removeNote = (event) => {
-  const notToRemove = event.target.parentNode;
-  sectionListNotes.removeChild(notToRemove);
-
-  //listNotes = listNotes.filter(note => note !== )
-};
+const formatDate = (date) => Intl.DateTimeFormat(navigator.language, {dateStyle: "short", timeStyle: "short"}).format(date); // a data foi formatada para o formato local do navegador
 
 const createNewNoteElement = (newNote) => {
   const newNoteElement = document.createElement("article");
+
+  const pDateElement = document.createElement("p");
+  pDateElement.textContent = formatDate(newNote.date)
+  newNoteElement.appendChild(pDateElement);
+
   const pElement = document.createElement("p");
-  pElement.textContent = newNote;
+  pElement.textContent = newNote.text;
   newNoteElement.appendChild(pElement);
 
   const trashElement = document.createElement("span");
   trashElement.className = "material-icons";
   trashElement.textContent = "delete_forever";
 
-  trashElement.addEventListener("click", removeNote);
+  trashElement.addEventListener("click", (event) => removeNote(event, newNote.id));
 
   newNoteElement.appendChild(trashElement);
 
   return newNoteElement;
-};
+}
 
 const addNoteToList = (newNote) => {
   const newNoteElement = createNewNoteElement(newNote);
 
   sectionListNotes.appendChild(newNoteElement);
-};
+}
 
 const cleanForm = () => form.reset();
 
 const handleSubmit = (event) => {
   event.preventDefault();
 
-  //recuperar a nota digitada pelo usuario
+  const dateNow = new Date()
+
+  const newNote = { // para separar os atributos dos objetos use a vírgula
+    id: dateNow.getTime(),
+    date: dateNow,
+    text: inputCpf.value
+  }
+
+  //recuperar a nota digitada pelo usuário
   const textNewNote = inputCpf.value;
 
-  addNoteToList(textNewNote);
+  addNoteToList(newNote);
 
-  listNotes.push(textNewNote);
+  listNotes.push(newNote);
 
   cleanForm();
-};
+
+}
 
 form.addEventListener("submit", handleSubmit);
